7cdbe75290f6a915a8290db902f45cef9ac00216
dhruvvanjara <dvanjara@tulip-tech.com>
2022-08-23 16:45:36 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,6 @@ export default ({ mode }: { mode: 'production' | 'development' | 'test' }) => {
     server: {
       port: 8080,
       hmr: {
-        host: '3.9.223.212',
         port: 22,
         protocol: 'wss'
       },
